1f130147309c9592892d37810a73b3c016eae898
Elliot Cho <57870635+echochs1@users.noreply.github.com>
2023-12-06 15:40:52 -0500

    Revert "Connected react router with different page components"

--- src/Overview.jsx
+++ src/Overview.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Box from '@mui/system/Box';
 
 const Overview = () => {
 
   return (
-    <Box>
-      <h1>Here we are in the Overview Page</h1>
-    </Box>
+    <div>
+      <h1>Overview</h1>
+    </div>
   );
 };
 
