9132488b065cbc3b6a999c45d612d8b48ec6d3ff
Lucy Wu <15126696+lwu15@users.noreply.github.com>
2023-12-07 11:56:45 -0500

    clean up Connections page code

--- src/pages/Connections.jsx
+++ src/pages/Connections.jsx
@@ -9,52 +9,6 @@ const Connections = () => {
     marginTop: '30px',
   };
 
-  const [formData, setFormData] = React.useState({
-    serverURI: '',
-    apiKey: '',
-    apiSecret: '',
-  });
-
-  // handle change for form input
-  function handleChange(event) {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  }
-
-  // handle submit
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      // API Request to create a connection, TO BE UPDATED
-      const response = await fetch('/api/createConnection', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          serverURI: formData.serverURI,
-          apiKey: formData.apiKey,
-          apiSecret: formData.apiSecret,
-        }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-      } else {
-        console.log('Failed to save credentials');
-      }
-    } catch (error) {
-      console.log('Error in CredentialForm: ', error);
-    } finally {
-      setSubmitting(false);
-      setFormData({ serverURI: '', apiKey: '', apiSecret: '' });
-    }
-  };
-
   return (
     <Container sx={containerStyle}>
       <div>
