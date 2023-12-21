3ce3b387fa903eaff17ab5c59f02927ea4bf88a2
Hudson McLane <hud.mclane@gmail.com>
2023-12-21 16:00:20 -0500

    add latest dashboard config with dynamic prometheus UID

--- src/GraphComponents/Graph.jsx
+++ src/GraphComponents/Graph.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import Box from "@mui/material/Box";
+import React from 'react';
+import Box from '@mui/material/Box';
 
 function Graph({ id }) {
-
   return (
     <>
       <iframe
         // an iframe with a specific URL based on the provided ID
-        src={`http://localhost:3000/d-solo/eaad4742-52a8-44c1-a53b-d37453c47986/prometheus-dashboard-test?orgId=1&refresh=5s&panelId=${id}`}
-        frameborder="0"
-        style={{pointerEvents: 'none', flexGrow: 1}}
+        src={`http://localhost:3000/d/ab97a8cb-08c6-4f4b-a81e-3cf22b397855/dashboard?orgId=1&var-PrometheusUID=PBFA97CFB590B2093&theme=dark&viewPanel=${id}`}
+        frameborder='0'
+        style={{ pointerEvents: 'none', flexGrow: 1 }}
       ></iframe>
     </>
   );
