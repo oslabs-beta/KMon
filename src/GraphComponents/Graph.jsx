b990e5007b1e28dd583beeec674b427e7e0e2872
Hudson McLane <hud.mclane@gmail.com>
2023-12-20 19:38:12 -0500

    update iframe URL in Graph component to reflect correct settings

--- src/GraphComponents/Graph.jsx
+++ src/GraphComponents/Graph.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
-function Graph({id}) {
-    
- return(
-  <Box>
-    <iframe
-      src={`http://localhost:3000/d-solo/eaad4742-52a8-44c1-a53b-d37453c47986/prometheus-dashboard-test?orgId=1&refresh=5s&panelId=${id}`}
-      width='450'
-      height='200'
-      frameborder='0'
-    >
-    </iframe>
-  </Box>
- )
+function Graph({ id }) {
+  return (
+    <Box>
+      <iframe
+        src={`http://localhost:3000/d/ab97a8cb-08c6-4f4b-a81e-3cf22b397855/dashboard?orgId=1&var-PrometheusUID=PBFA97CFB590B2093&theme=dark&viewPanel=${id}`}
+        width='450'
+        height='200'
+        frameborder='0'
+      ></iframe>
+    </Box>
+  );
 }
 
 export default Graph;
